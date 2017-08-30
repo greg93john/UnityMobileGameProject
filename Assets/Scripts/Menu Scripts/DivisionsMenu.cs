5af0b5caae1405c43b80a249b76551cbece5754b
Greg Johnson <greg93John@gmail.com>
2017-08-30 19:12:49 -0400

    Updates game to a full functional mobile build
    Updates build of mobile game to a n early prototype of the mobile game.
    (selecting Item and Ability slots are not functional in gameplay yet).

---
+++ Assets/Scripts/Menu Scripts/DivisionsMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DivisionsMenu : MonoBehaviour {
+
+    public Text lightText, midText, heavyText;
+    public Color lightTextOriginalColor, midTextOriginalColor, heavyTextOriginalColor;
+
+    private void OnEnable() {
+        SetAllTextToOriginalColor();
+        HighlightChosenDivision();
+    }
+
+    private void SetAllTextToOriginalColor() {
+        lightText.color = lightTextOriginalColor;
+        midText.color = midTextOriginalColor;
+        heavyText.color = heavyTextOriginalColor;
+    }
+
+    private void HighlightChosenDivision() {
+        string activeDivision = GameObject.FindObjectOfType<LevelSceneInfo>().GetDivisionChoice();
+
+        switch (activeDivision) {
+            case "light": { lightText.color = Color.white; }
+                break;
+            case "mid": { midText.color = Color.white; }
+                break;
+            case "heavy": { heavyText.color = Color.white; }
+                break;
+            default: { Debug.Log("No division is currently selected"); }
+                break;
+        }
+    }
+}
--- Assets/Scripts/PlayerStatMeta.cs
+++ Assets/Scripts/PlayerStatMeta.cs
@@ -2,7 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStatMeta {
+public class PlayerStatMeta : MonoBehaviour {
+
+    private static Object instance;
+    private void Start() {
+        if (instance) { Destroy(this.gameObject); } else { instance = this; DontDestroyOnLoad(this.gameObject); }
+    }
+
+    // Player Stats
+
     const string playerMaxHealthKey = "PLAYER_MAX_HEALTH", playerDefenseKey = "PLAYER_DEFENSE", playerAttackPowerKey = "PLAYER_ATTACK_POWER";
 
     public static void SetPlayerMaxHealthStat(int setValue) {
@@ -21,5 +29,180 @@ public class PlayerStatMeta {
         PlayerPrefs.SetInt(playerAttackPowerKey, setValue);
     } public static int GetPlayerAttackPowerStat() {
         return PlayerPrefs.GetInt(playerAttackPowerKey);
+    }
+
+    // Ability Stats
+
+    const string abilityOneKey = "ABILITY1", abilityTwoKey = "ABILITY2", abilityThreeKey = "ABILITY3";
+
+    public static void SetAbilityName(int slot, string abilityName) {
+        switch (slot) {
+            case 0: { PlayerPrefs.SetString(abilityOneKey, abilityName); }
+                break;
+            case 1: { PlayerPrefs.SetString(abilityTwoKey, abilityName); }
+                break;
+            case 2: { PlayerPrefs.SetString(abilityThreeKey, abilityName); }
+                break;
+        }
+    }
+    public static string GetAbilityName(int slot) {
+        switch (slot) {
+            case 0: { return PlayerPrefs.GetString(abilityOneKey); }
+            case 1: { return PlayerPrefs.GetString(abilityTwoKey); }
+            case 2: { return PlayerPrefs.GetString(abilityThreeKey); }
+            default: { Debug.LogWarning("Warning, ability slot index out of range: " + slot); return ""; }
+        }
+    }
+
+    public static void SetAbilityPotency(int slot, int potency) {
+        switch (slot) {
+            case 0: { PlayerPrefs.SetInt(abilityOneKey, potency); }
+                break;
+            case 1: { PlayerPrefs.SetInt(abilityTwoKey, potency); }
+                break;
+            case 2: { PlayerPrefs.SetInt(abilityThreeKey, potency); }
+                break;
+        }
+    }
+    public static int GetAbilityPotency(int slot) {
+        switch (slot) {
+            case 0: { return PlayerPrefs.GetInt(abilityOneKey); }
+            case 1: { return PlayerPrefs.GetInt(abilityTwoKey); }
+            case 2: { return PlayerPrefs.GetInt(abilityThreeKey); }
+            default: { Debug.LogWarning("Warning, ability slot index out of range: " + slot); return -1; }
+        }
+    }
+
+    public static void SetAbilityCoolDown(int slot, float coolDownTime) {
+        switch (slot) {
+            case 0: { PlayerPrefs.SetFloat(abilityOneKey, coolDownTime); }
+                break;
+            case 1: { PlayerPrefs.SetFloat(abilityTwoKey, coolDownTime); }
+                break;
+            case 2: { PlayerPrefs.SetFloat(abilityThreeKey, coolDownTime); }
+                break;
+        }
+    }
+    public static float GetAbilityCoolDown(int slot) {
+        switch (slot) {
+            case 0: { return PlayerPrefs.GetFloat(abilityOneKey); }
+            case 1: { return PlayerPrefs.GetFloat(abilityTwoKey); }
+            case 2: { return PlayerPrefs.GetFloat(abilityThreeKey); }
+            default: { Debug.LogWarning("Warning, ability slot index out of range: " + slot); return 0f; }
+        }
+    }
+
+    // Items
+
+    const string itemOneKey = "ITEM1", itemTwoKey = "ITEM2", itemThreeKey = "ITEM3";
+
+    public static void SetItemName(int slot, string itemName) {
+        switch (slot) {
+            case 0: { PlayerPrefs.SetString(itemOneKey, itemName); }
+                break;
+            case 1: { PlayerPrefs.SetString(itemTwoKey, itemName); }
+                break;
+            case 2: { PlayerPrefs.SetString(itemThreeKey, itemName); }
+                break;
+        }
+    }
+    public static string GetItemName(int slot) {
+        switch (slot) {
+            case 0: { return PlayerPrefs.GetString(itemOneKey); }
+            case 1: { return PlayerPrefs.GetString(itemTwoKey); }
+            case 2: { return PlayerPrefs.GetString(itemThreeKey); }
+            default: { Debug.LogWarning("Warning, item slot index out of range: " + slot); return ""; }
+        }
+    }
+
+    public static void SetItemPotency(int slot, int potency) {
+        switch (slot) {
+            case 0: { PlayerPrefs.SetInt(itemOneKey, potency); }
+                break;
+            case 1: { PlayerPrefs.SetInt(itemTwoKey, potency); }
+                break;
+            case 2: { PlayerPrefs.SetInt(itemThreeKey, potency); }
+                break;
+        }
+    }
+    public static int GetItemPotency(int slot) {
+        switch (slot) {
+            case 0: { return PlayerPrefs.GetInt(itemOneKey); }
+            case 1: { return PlayerPrefs.GetInt(itemTwoKey); }
+            case 2: { return PlayerPrefs.GetInt(itemThreeKey); }
+            default: { Debug.LogWarning("Warning, item slot index out of range: " + slot); return -1; }
+        }
+    }
+
+    public static void SetItemCoolDown(int slot, float coolDownTime) {
+        switch (slot) {
+            case 0: { PlayerPrefs.SetFloat(itemOneKey, coolDownTime); }
+                break;
+            case 1: { PlayerPrefs.SetFloat(itemTwoKey, coolDownTime); }
+                break;
+            case 2: { PlayerPrefs.SetFloat(itemThreeKey, coolDownTime); }
+                break;
+        }
+    }
+    public static float GetItemCoolDown(int slot) {
+        switch (slot) {
+            case 0: { return PlayerPrefs.GetFloat(itemOneKey); }
+            case 1: { return PlayerPrefs.GetFloat(itemTwoKey); }
+            case 2: { return PlayerPrefs.GetFloat(itemThreeKey); }
+            default: { Debug.LogWarning("Warning, item slot index out of range: " + slot); return 0f; }
+        }
+    }
+
+    public static void SetItemMaxReserve(int slot, int maxReserve) {
+        switch (slot) {
+            case 0: { PlayerPrefs.SetInt(itemOneKey, maxReserve); }
+                break;
+            case 1: { PlayerPrefs.SetInt(itemTwoKey, maxReserve); }
+                break;
+            case 2: { PlayerPrefs.SetInt(itemThreeKey, maxReserve); }
+                break;
+        }
+    }
+    public static int GetItemMaxReserve(int slot) {
+        switch (slot) {
+            case 0: { return PlayerPrefs.GetInt(itemOneKey); }
+            case 1: { return PlayerPrefs.GetInt(itemTwoKey); }
+            case 2: { return PlayerPrefs.GetInt(itemThreeKey); }
+            default: { Debug.LogWarning("Warning, item slot index out of range: " + slot); return 0; }
+        }
+    }
+
+    // Currency
+
+    const string currencyKey = "CURRENCY";
+
+    public static int GetCurrentReserveAmount() {
+        return PlayerPrefs.GetInt(currencyKey);
+    }
+
+    public static void AddToReserve(int incomingAmount) {
+        int totalReserve = incomingAmount + GetCurrentReserveAmount();
+        PlayerPrefs.SetInt(currencyKey, totalReserve);
+    }
+    public static void ReduceFromReserve(int reductionValue) {
+        int totalReserve = GetCurrentReserveAmount() - reductionValue;
+        PlayerPrefs.SetInt(currencyKey, totalReserve);
+    }
+
+    public static void HardSetReserveAmountTo(int total) {
+        PlayerPrefs.SetInt(currencyKey, total);
+    }
+
+    // Reset All
+
+    public static int defaultPlayerAttack = 1, defaultPlayerDefense = 1, defaultPlayerMaxHealth = 10;
+
+    void ResetAllMeta(bool answerVal) {
+        if (answerVal) {
+            SetPlayerAttackPowerStat(defaultPlayerAttack);
+            SetPlayerDefenseStat(defaultPlayerDefense);
+            SetPlayerMaxHealthStat(defaultPlayerMaxHealth);
+            HardSetReserveAmountTo(0);
+        }
     }
 }
